7d714e20eb3eec0fbc8811bdf4a67022eea5aa8e
Francisco Javier Rossello Jeronimo <frossellojeronimo@cifpfbmoll.eu>
2024-10-10 21:40:36 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+// En este Kata vamos a generar una funcion que clasifique los numeros en pares "Even" o impares "Odd"
+public class Kata {
+    public static String evenOrOdd(int number) {
+      if (number % 2 == 0){
+        return "Even";
+      }
+        else return "Odd";
+    }
+}
